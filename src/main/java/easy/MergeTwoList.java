d5009c9314949430a15c4839d0051377140d18e1
Zaidi Zainal <zaidi.asyraf@yahoo.com>
2022-07-11 14:57:32 +0800

    wip merge two list

---
+++ src/main/java/easy/MergeTwoList.java
@@ -0,0 +1,16 @@
+package easy;
+
+import easy.datastructure.ListNode;
+
+public class MergeTwoList {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode listNode = new ListNode();
+        ListNode[] listNodes = new ListNode[]{};
+
+    }
+}
---
+++ src/main/java/easy/datastructure/ListNode.java
@@ -0,0 +1,17 @@
+package easy.datastructure;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    public ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
